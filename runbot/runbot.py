fde297c1496e7091ee76e43a8c8045a89f25f34a
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2015-07-22 08:38:57 +0200

    XXX WIP temporarily rebuild old sticky branches

--- runbot/runbot.py
+++ runbot/runbot.py
@@ -289,7 +289,7 @@ class runbot_repo(osv.osv):
                 branch_id = Branch.create(cr, uid, {'repo_id': repo.id, 'name': name})
             branch = Branch.browse(cr, uid, [branch_id], context=context)[0]
             # skip build for old branches
-            if dateutil.parser.parse(date[:19]) + datetime.timedelta(30) < datetime.datetime.now():
+            if not branch.sticky and dateutil.parser.parse(date[:19]) + datetime.timedelta(30) < datetime.datetime.now():
                 continue
             # create build (and mark previous builds as skipped) if not found
             build_ids = Build.search(cr, uid, [('branch_id', '=', branch.id), ('name', '=', sha)])
